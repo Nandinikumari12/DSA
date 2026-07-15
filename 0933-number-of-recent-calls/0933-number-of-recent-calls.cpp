d7b7c3f8d9dfbad0670b6698e6508e0b9ca7803d
Nandinikumari12 <149036423+Nandinikumari12@users.noreply.github.com>
2026-07-15 16:42:30 +0530

    Time: 12 ms (80.6%), Space: 64.1 MB (67.65%) - LeetHub

---
+++ 0933-number-of-recent-calls/0933-number-of-recent-calls.cpp
@@ -0,0 +1,20 @@
+#include <queue>
+using namespace std;
+
+class RecentCounter {
+public:
+    queue<int> q;
+
+    RecentCounter() {}
+
+    int ping(int t) {
+        q.push(t);
+
+        // Remove outdated requests
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+
+        return q.size();
+    }
+};
